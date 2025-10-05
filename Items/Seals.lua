723b6a547fb4321f2f1d4c31480040dcb45512fa
mrpoattomna <superpotato277@gmail.com>
2025-10-05 22:20:12 +0300

    e

--- Items/Seals.lua
+++ Items/Seals.lua
@@ -36,7 +36,7 @@ SMODS.Seal{
         return{vars = {card.ability.seal.extra.retriggers, numerator1 , denominator1, numerator2, denominator2}}
     end,
     calculate = function (self, card, context)
-        if context.after and SMODS.pseudorandom_probability(card, "xmpl_epik_face", 1, card.ability.seal.extra.odds1) then
+        if context.after and context.cardarea == G.play and SMODS.pseudorandom_probability(card, "xmpl_epik_face", 1, card.ability.seal.extra.odds1) then
             for i = 1, #G.hand.cards do
                 SMODS.destroy_cards(G.hand.cards[i])
             end
@@ -45,7 +45,7 @@ SMODS.Seal{
                 sound = "xmpl_bruh"
             }
         end
-        if context.after and SMODS.pseudorandom_probability(card, "xmpl_epik_face", 1, card.ability.seal.extra.odds2) then
+        if context.after and context.cardarea == G.play and SMODS.pseudorandom_probability(card, "xmpl_epik_face", 1, card.ability.seal.extra.odds2) then
             SMODS.destroy_cards(card)
             return{
                 message = "EPIK OUTRO",
